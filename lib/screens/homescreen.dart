bd03a0b78b6e8ca1a534320f199fa680f28321eb
matheusDev <matheus.info29@gmail.com>
2024-03-14 07:49:05 -0300

    Criaçãpo do carrosel do menu inicial

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -12,6 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int activeIndex = 0;
   final urlImages = [
     'https://images.unsplash.com/photo-1511919884226-fd3cad34687c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1542362567-b07e54358753?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -28,12 +29,30 @@ class _HomeScreenState extends State<HomeScreen> {
             CarouselSlider.builder(
                 itemCount: urlImages.length,
                 itemBuilder: (context, index, realIndex) {
-                  final urlImages = urlImages
+                  final urlImage = urlImages[index];
+                  return buildImage(urlImage, index);
                 },
-                options: CarouselOptions(height: 400))
+                options: CarouselOptions(
+                    onPageChanged: (index, reason) =>
+                        setState(() => activeIndex = index),
+                    height: 300,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 5),
+                    enlargeCenterPage: true,
+                    enlargeStrategy: CenterPageEnlargeStrategy.height)),
+            const SizedBox(height: 32),
           ],
         ),
       ),
     );
   }
 }
+
+Widget buildImage(String urlImage, int index) => Container(
+      margin: const EdgeInsets.symmetric(horizontal: 12),
+      color: Colors.grey,
+      child: Image.network(
+        urlImage,
+        fit: BoxFit.cover,
+      ),
+    );
