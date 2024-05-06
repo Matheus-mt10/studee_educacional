ab01e5e305056d38e597522741d3ab6d38e50dca
matheusDev <matheus.info29@gmail.com>
2024-05-06 08:01:21 -0300

    Update

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 import 'package:studee_educational/components/basescaffold_screen.dart';
 
 class NoticationsScreen extends StatefulWidget {
@@ -20,30 +21,42 @@ class _NoticationsScreenState extends State<NoticationsScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                width: double.infinity,
-                height: 80,
-                child: Container(
-                    decoration: BoxDecoration(color: Colors.black),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        InkWell(
-                            onTap: () {},
-                            child: Icon(
-                              FontAwesomeIcons.arrowLeft,
-                              color: Colors.white,
-                            )),
-                        Text(
-                          'Notificações',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        )
-                      ],
-                    )),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 80,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        child: SizedBox(
+                          height: 30,
+                          width: 30,
+                          child: InkWell(
+                              onTap: () {
+                                context.push('/homepage');
+                              },
+                              child: Icon(
+                                FontAwesomeIcons.arrowLeft,
+                                color: Colors.black,
+                                size: 14,
+                              )),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Notificações',
+                        style: TextStyle(color: Colors.black, fontSize: 26),
+                      )
+                    ],
+                  ),
+                ),
               ),
-              Divider(),
               SizedBox(
                 height: 20,
               ),
