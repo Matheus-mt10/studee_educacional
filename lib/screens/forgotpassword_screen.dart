ac752acaed419fdc22f622c6d55214b615010cb2
matheusDev <matheus.info29@gmail.com>
2024-05-26 11:38:47 -0300

    Update

--- lib/screens/forgotpassword_screen.dart
+++ lib/screens/forgotpassword_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:studee_educational/components/basescaffold_screen.dart';
 import 'package:studee_educational/components/input.dart';
@@ -34,12 +35,95 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                 style: TextStyle(fontSize: 16),
               ),
               SizedBox(
-                height: 40,
+                height: 36,
               ),
               Input(label: 'Email', internalText: 'Digite o seu Email'),
-              FilledButton(onPressed: () {}, child: Text("Continuar"))
+              FilledButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) => CustomAlertDialog());
+                  },
+                  child: Text("Continuar")),
+              TextButton.icon(
+                  icon: Icon(
+                    FontAwesomeIcons.chevronLeft,
+                    size: 16,
+                  ),
+                  onPressed: () {
+                    context.go('/');
+                  },
+                  label: Text("Voltar para o login"))
             ],
           ),
         ));
+  }
+}
+
+class CustomAlertDialog extends StatelessWidget {
+  const CustomAlertDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(2)),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'images/AlertIcon.png',
+                scale: 5,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Foi envido um link com as informações para redefinição no email informado",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  WidgetStatePropertyAll(Colors.indigo)),
+                          onPressed: () {
+                            context.go('/');
+                          },
+                          child: Text(
+                            "Entendi",
+                            style: TextStyle(color: Colors.white),
+                          ))),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor:
+                                WidgetStatePropertyAll(Colors.indigo)),
+                        onPressed: () {},
+                        child: Text(
+                          "Enviar novamente",
+                          style: TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
+                        )),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
