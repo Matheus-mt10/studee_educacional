ee67bfbdab4983a3a6e31465a968f4c2ce6ea0a6
matheusDev <matheus.info29@gmail.com>
2024-05-29 13:17:43 -0300

    Update

--- lib/screens/business_classes_screen.dart
+++ lib/screens/business_classes_screen.dart
@@ -22,34 +22,83 @@ class _BusinessClassesScreenState extends State<BusinessClassesScreen> {
       body: Padding(
         padding: const EdgeInsets.all(14.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "Aulas de Business",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(
+                "Aulas de Business",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
             ),
-            Container(
-              child: Column(
-                children: [
-                  Row(
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text("hghgjhgjg"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Container(
+                  child: Column(
                     children: [
-                      Text(
-                        "09:00 am",
-                        style: TextStyle(fontWeight: FontWeight.w600),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "09:00 am",
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                          Icon(
+                            FontAwesomeIcons.solidBell,
+                            size: 16,
+                          )
+                        ],
                       ),
-                      Icon(FontAwesomeIcons.solidBell)
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Text(
+                          "Vamos explorar os fundamentos dos negócios, abordando planejamento estratégico, análise de mercado, marketing, finanças e gestão de recursos humanos. Nosso objetivo é oferecer uma visão ampla e integrada do ambiente empresarial, utilizando casos práticos e teorias relevantes.",
+                          textAlign: TextAlign.justify,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 14,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Sala 14',
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                          Icon(
+                            FontAwesomeIcons.locationDot,
+                            size: 16,
+                          ),
+                          SizedBox(
+                            width: 200,
+                          ),
+                          Icon(
+                            FontAwesomeIcons.solidClock,
+                            size: 16,
+                          ),
+                          Text(
+                            '1:30h',
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      )
                     ],
                   ),
-                  Text(
-                      "Vamos explorar os fundamentos dos negócios, abordando planejamento estratégico, análise de mercado, marketing, finanças e gestão de recursos humanos. Nosso objetivo é oferecer uma visão ampla e integrada do ambiente empresarial, utilizando casos práticos e teorias relevantes."),
-                  Row(
-                    children: [
-                      Text('Sala 14'),
-                      Icon(FontAwesomeIcons.locationDot),
-                      Icon(FontAwesomeIcons.solidClock),
-                      Text('1:30h')
-                    ],
-                  )
-                ],
+                ),
               ),
             )
           ],
